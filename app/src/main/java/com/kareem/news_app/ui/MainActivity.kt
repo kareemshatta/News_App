d03c2813699b18b4f8101f7cf516db90e458a371
kareemshatta <kareemshatta3@gmail.com>
2022-01-11 23:45:09 +0200

    first commit

---
+++ app/src/main/java/com/kareem/news_app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kareem.news_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kareem.news_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kareem/news_app/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.kareem.news_app.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
